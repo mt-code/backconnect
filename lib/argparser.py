459450235e756b2febe6e61a173e5a3e19bd4b34
mt-code <matt@mtcode.co.uk>
2022-04-18 21:56:52 +0100

    refactor: Added periods to the argument help copy

--- lib/argparser.py
+++ lib/argparser.py
@@ -14,24 +14,24 @@ class ArgParser:
                                               add_help=False)
 
         required_args = self.parser.add_argument_group("required arguments")
-        required_args.add_argument('-u', '--url', help='The target URL')
-        required_args.add_argument('-i', '--ip', help='The local listener IP')
-        required_args.add_argument('-p', '--port', help='The local listener port')
+        required_args.add_argument('-u', '--url', help='The target URL.')
+        required_args.add_argument('-i', '--ip', help='The local listener IP.')
+        required_args.add_argument('-p', '--port', help='The local listener port.')
 
         optional_args = self.parser.add_argument_group("optional arguments")
-        optional_args.add_argument('-d', '--data', help='POST data query string (e.g. user=admin&pass=admin)')
-        optional_args.add_argument('-h', '--help', help='Show this help information and quit', action='store_true')
+        optional_args.add_argument('-d', '--data', help='POST data query string. (e.g. user=admin&pass=admin)')
+        optional_args.add_argument('-h', '--help', help='Show this help information and quit.', action='store_true')
         optional_args.add_argument('--interactive',
                                    help='Starts in interactive mode, can be used in conjunction with other '
-                                        'arguments',
+                                        'arguments.',
                                    action='store_true', )
-        optional_args.add_argument('--list', help='Lists all of the available payloads', action='store_true')
+        optional_args.add_argument('--list', help='Lists all of the available payloads.', action='store_true')
         optional_args.add_argument('--payloads',
                                    help='Comma separated list of payloads to use, if no payloads are specified '
-                                        'all of the available payloads are attempted')
+                                        'all of the available payloads are attempted.')
         optional_args.add_argument('--view',
                                    help='View a specific payload, when --ip and --port are provided the payload is '
-                                        'populated with these values',
+                                        'populated with these values.',
                                    metavar='PAYLOAD')
 
         try:
